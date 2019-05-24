284b87a3bc53409dddb267d9fef981e3b5dc0287
Diego Romario <diego.romario@outlook.com>
2019-05-23 22:24:20 -0300

    Adicionando FA

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
 
 
 @NgModule({
-    declarations: [ PhotoComponent,
-                    PhotoListComponent,
-                    PhotoFormComponent,
-                    PhotosComponent,
-                    FilterByDescription,
-                    LoadButtonComponent
-    ], // PRIVADO
+    declarations: [], // PRIVADO
     // exports: [ PhotoComponent ], // PUBLICO - É NECESSÁRIO DEFINIR AQUI O COMPONENTE QUANDO O MESMO FOR UTILIZADO EM OUTRO MODULO
-    imports: [ HttpClientModule,  // IMPORTANDO O HTTPCLIENT MODULE ATRAVES DO MODULO QUE REALMENTE NECESSITA
-        CommonModule ] // IMPORTAR O COMMONMODULO PARA TORNAR DISPONÍVEL AS DIRETIVAS NATIVAS DO ANGULAR
+    imports: [
+        PhotoModule,
+        PhotoListModule,
+        PhotoFormModule,
+        CommonModule,
+    ]
 })
 
-export class PhotosModule {}
+export class PhotosModule { }
