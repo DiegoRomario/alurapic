0e33ab2891c4b1c085ae76c1f18be7a3fdcd2d47
Diego Romario <diego.romario@outlook.com>
2020-12-16 21:15:03 -0300

    Adicionando testes e2e para cadastro e login

--- e2e/src/signin.e2e-spec.ts
+++ e2e/src/signin.e2e-spec.ts
@@ -1,18 +1,39 @@
-import { SigninPage } from './signin.po';
+import { SigninPage } from './pageObjects/signin.po';
+import { SignUpPage } from './pageObjects/signup.po';
 
 describe('Testando tela home', () => {
   let signinPage: SigninPage;
-
+  let signupPage: SignUpPage;
+  const informacoesDoUsuario = [
+    { nome: 'email', valor: 'diego@diego.com' },
+    { nome: 'fullName', valor: 'Diego Romário' },
+    { nome: 'userName', valor: 'diegoro12' },
+    { nome: 'password', valor: '12345678' }
+  ];
   beforeEach(() => {
     signinPage = new SigninPage();
+    signupPage = new SignUpPage();
   });
 
-  it('Deve ir para home', () => {
-    signinPage.acessarHome();
+  it('Dever navegar para Signup', () => {
+    signupPage.navegarParaSignUp();
+  });
+
+  it('Dever cadastrar Usuario', () => {
+    informacoesDoUsuario.forEach(usuario => {
+      signupPage.registraUsuario(usuario.nome, usuario.valor);
+    });
+    expect(signupPage.pegarBotaoRegister().click());
   });
 
   it('Deve verificar url', () => {
     expect(signinPage.verificarUrl()).toEqual('http://localhost:4200/#/home');
+  });
+
+  it('Deve fazer login', () => {
+    expect(signinPage.pegarInput('userName', 'flavio'));
+    expect(signinPage.pegarInput('password', '123'));
+    expect(signinPage.pegarBotaoLogin().click());
   });
 
 });
